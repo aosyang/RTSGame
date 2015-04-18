dd85cdc8d839adc725786cf5c78c774334285d06
Shiyang Ao <aosyang@hotmail.com>
2015-04-18 19:09:51 -0400

    Submitted project files to the repository.

---
+++ Assets/Scritps/BaseVehicle.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseVehicle : MonoBehaviour
+{
+    int terrainLayer = -1;
+
+    public float vehicleSpeed = 5.0f;
+
+    Vector3 targetPoint;
+    bool moving = false;
+
+    Vector3 lastFramePosition;
+    
+    // Use this for initialization
+    void Start()
+    {
+        terrainLayer = 1 << LayerMask.NameToLayer("Terrain");
+        lastFramePosition = transform.position - new Vector3(0.0f, 0.0f, 1.0f);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving)
+        {
+            Vector3 deltaPos = targetPoint - transform.position;
+            deltaPos.y = 0.0f;
+            float planarDist = deltaPos.sqrMagnitude;
+            deltaPos = deltaPos.normalized * Time.deltaTime * vehicleSpeed;
+            
+            if (deltaPos.sqrMagnitude < planarDist)
+                transform.position = transform.position + deltaPos;
+            else
+            {
+                Vector3 targetGroundPos = targetPoint;
+                targetGroundPos.y += GetHeight();
+                transform.position = targetGroundPos;
+                moving = false;
+            }
+        }
+
+        // Put vehicle on the ground
+        Ray downCastRay = new Ray(transform.position + new Vector3(0.0f, 1.0f, 0.0f), Vector3.down);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(downCastRay, out hitInfo, 1000.0f, terrainLayer))
+        {
+            Vector3 groundPos = hitInfo.point;
+            groundPos.y += GetHeight();
+            transform.position = groundPos;
+
+            //Vector3 planarTarget = groundPos + Vector3.ProjectOnPlane(targetPoint - groundPos, hitInfo.normal);
+            Vector3 planarTarget = groundPos - lastFramePosition + groundPos;
+            transform.LookAt(planarTarget, hitInfo.normal);
+
+            lastFramePosition = transform.position;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Ray screenRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(screenRay, out hitInfo, 1000.0f, terrainLayer))
+            {
+                targetPoint = hitInfo.point;
+                moving = true;
+            }
+        }
+
+    }
+
+    float GetHeight()
+    {
+        return 0.0f;
+    }
+}
---
+++ Assets/Scritps/CameraScroller.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScroller : MonoBehaviour
+{
+    public float minX = 0.0f, maxX = 50.0f, minY = 0.0f, maxY = 50.0f;
+    public float scrollSpeed = 50.0f;
+    public float scrollPercentage = 0.05f;
+
+    // Use this for initialization
+    void Start()
+    {
+    
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 viewPoint = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        Vector3 nodePos = Vector3.zero;
+
+        if (viewPoint.x < scrollPercentage)
+            nodePos.x -= 1.0f;
+
+        if (viewPoint.x > 1.0f - scrollPercentage)
+            nodePos.x += 1.0f;
+
+        if (viewPoint.y < scrollPercentage)
+            nodePos.z -= 1.0f;
+
+        if (viewPoint.y > 1.0f - scrollPercentage)
+            nodePos.z += 1.0f;
+
+        Vector3 boundPos = transform.position + nodePos.normalized * Time.deltaTime * scrollSpeed;
+
+        if (boundPos.x < minX)
+            boundPos.x = minX;
+
+        if (boundPos.x > maxX)
+            boundPos.x = maxX;
+
+        if (boundPos.z < minY)
+            boundPos.z = minY;
+
+        if (boundPos.z > maxY)
+            boundPos.z = maxY;
+
+        transform.position = boundPos;
+    }
+}
