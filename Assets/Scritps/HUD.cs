a02aa506758afd006f0a98e345e4b10ec024eab2
Mwolf <aosyang@hotmail.com>
2015-04-19 10:25:56 -0400

    Fixed selection issues
    Game:
    [HUD] Fixed selection box rendering.
    [HUD] Fixed selection not working when user drag selection box in an
    inverse order.
    
    General:
    [Git] Added Temp to ignore list.

--- Assets/Scritps/HUD.cs
+++ Assets/Scritps/HUD.cs
@@ -17,24 +17,31 @@ public class HUD : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		if (Input.GetMouseButtonDown (0)) {
+		if (Input.GetMouseButtonDown (0))
+		{
 			boxSelecting = true;
 			selStartPos = Input.mousePosition;
-		} else if (Input.GetMouseButtonUp (0)) {
+		}
+		else if (Input.GetMouseButtonUp (0))
+		{
 			boxSelecting = false;
 
-			Rect selection = new Rect(selStartPos.x,
-			                          Camera.main.pixelRect.height - selStartPos.y,
-			                          Input.mousePosition.x - selStartPos.x,
-			                          selStartPos.y - Input.mousePosition.y);
-			//Debug.Log(selection);
+			// Make a Rect with selected range, then we project vehicles' positions
+			// to the screen and check if they are inside the Rect.
+
+			float x1 = selStartPos.x, y1 = selStartPos.y,
+				  x2 = Input.mousePosition.x, y2 = Input.mousePosition.y;
+			if (x1 > x2) Swap(ref x1, ref x2);
+			if (y1 > y2) Swap(ref y1, ref y2);
+
+			Rect selection = new Rect(x1, y1, x2 - x1, y2 - y1);
+
 			List<BaseVehicle> vehicleList = new List<BaseVehicle>();
 
 			foreach (BaseVehicle v in GameObject.FindObjectsOfType<BaseVehicle>())
 			{
 				Vector2 screenPoint = Camera.main.WorldToScreenPoint(v.transform.position);
-				screenPoint.y = Camera.main.pixelRect.height - screenPoint.y;
-				//Debug.Log (screenPoint);
+
 				if (selection.Contains(screenPoint))
 				{
 					vehicleList.Add(v);
@@ -53,13 +60,25 @@ public class HUD : MonoBehaviour
 			GUI.Box(new Rect(pos.x - 50, Camera.main.pixelRect.height - pos.y + 20, 100, 20), v.GetTypeName());
 		}
 
-		if (boxSelecting) {
-			Rect rect = new Rect(selStartPos.x,
-			                     Camera.main.pixelRect.height - selStartPos.y,
-			                     Input.mousePosition.x - selStartPos.x,
-			                     selStartPos.y - Input.mousePosition.y);
+		if (boxSelecting)
+		{
+			float x1 = selStartPos.x, y1 = selStartPos.y,
+				  x2 = Input.mousePosition.x, y2 = Input.mousePosition.y;
+
+			// Swap values in case we have negative width and/or height in Rect
+			if (x1 > x2) Swap(ref x1, ref x2);
+			if (y1 < y2) Swap(ref y1, ref y2);
+
+			Rect rect = new Rect(x1, Camera.main.pixelRect.height - y1, x2 - x1, y1 - y2);
 			GUI.Box(rect, "");
 			//Debug.Log(rect);
 		}
 	}
+
+	public static void Swap<T> (ref T lhs, ref T rhs) {
+		T temp = lhs;
+		lhs = rhs;
+		rhs = temp;
+	}
+
 }
